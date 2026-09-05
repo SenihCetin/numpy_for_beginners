1b18a48428e75a36e43c7890d041a6858ee17979
Senih Çetin <ahmet.senih.cetin@gmail.com>
2026-09-05 17:33:26 +0300

    Update 3rd_slicing.py

--- numpy/3rd_slicing.py
+++ numpy/3rd_slicing.py
@@ -5,9 +5,9 @@ array = np.array([[1 ,2 ,3 ,4],
                   [9 ,10 ,11 ,12],
                   [13 ,14 ,15 ,16]])
 
-#For slicing there is 3 way.
+#  For slicing there is 3 way.
 
-#array[start index : end index : step
+#  array[start index : end index : step size]
 print(array[0])
 print(array[1])
 print(array[-1])          #It will print last row
@@ -15,7 +15,7 @@ print(array[0 : 2])       #It gives 0 through 1 not 2. Because the ending index 
 print(array[3 : 4])       #To see last row we have to write one more than the last index.
 print(array[0 : 4 : 2])
 print(array[:: -1])
-#array[start row's index : end row's index, start column's index : end column's index : step]
+#  array[start row's index : end row's index, start column's index : end column's index : step]
 print(array[0 : 2, 1 : 3])
 print(array[: ,::2])
 print(array[:: , 1::2])
