f7c26d379b36a358fbea1b9164f469b2214bdc67
Senih Çetin <ahmet.senih.cetin@gmail.com>
2026-09-05 17:32:18 +0300

    Update 3rd_slicing.py

--- numpy/3rd_slicing.py
+++ numpy/3rd_slicing.py
@@ -10,9 +10,9 @@ array = np.array([[1 ,2 ,3 ,4],
 #array[start index : end index : step
 print(array[0])
 print(array[1])
-print(array[-1])    #It will print last row
-print(array[0 : 2]) #It gives 0 through 1 not 2. Because the ending index is exclusive.
-print(array[3 : 4]) #To see last row we have to write one more than the last index.
+print(array[-1])          #It will print last row
+print(array[0 : 2])       #It gives 0 through 1 not 2. Because the ending index is exclusive.
+print(array[3 : 4])       #To see last row we have to write one more than the last index.
 print(array[0 : 4 : 2])
 print(array[:: -1])
 #array[start row's index : end row's index, start column's index : end column's index : step]
